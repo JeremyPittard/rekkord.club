724387b383bd6a4fb1ca52c5d8c957504999ae9e
Jeremy Pittard <jeremy@jpittard.net>
2024-03-04 13:09:50 +0800

    create user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,7 +1,16 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  {
+    timespamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
